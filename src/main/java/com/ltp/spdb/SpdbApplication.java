69a74272ca00e588c7924104d457ed794f6ae521
Googodme <googodme@gmail.com>
2022-06-25 20:25:34 +0530

    Empty Project

---
+++ src/main/java/com/ltp/spdb/SpdbApplication.java
@@ -0,0 +1,13 @@
+package com.ltp.spdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpdbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpdbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ltp/spdb/SpdbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ltp.spdb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpdbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
